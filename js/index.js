d76d351246a46a3aa70d59a300aed090396aeee4
Eugene Kortelyov <eugene.kortelyov@gmail.com>
2023-10-20 01:03:15 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -17,9 +17,6 @@ let btn04 = document.getElementById("btn04");
 let btn11 = document.getElementById("btn11");
 let btn12 = document.getElementById("btn12");
 let btn13 = document.getElementById("btn13");
-let btn14 = document.getElementById("btn14");
-let btn15 = document.getElementById("btn15");
-let btn16 = document.getElementById("btn16");
 
 let btn21 = document.getElementById("btn21");
 let btn22 = document.getElementById("btn22");
@@ -29,87 +26,79 @@ let btn25 = document.getElementById("btn25");
 let btn26 = document.getElementById("btn26");
 
 let btn31 = document.getElementById("btn31");
+let btn32 = document.getElementById("btn32");
+let btn33 = document.getElementById("btn33");
+let btn34 = document.getElementById("btn34");
+let btn35 = document.getElementById("btn35");
+let btn36 = document.getElementById("btn36");
 
-let btn42 = document.getElementById("btn42");
+let btn41 = document.getElementById("btn41");
 
 btn01.addEventListener("click", function(){
     window.location = 'index_flight_type.html';
 });
-
 btn02.addEventListener("click", function(){
     window.location = 'index_registration.html';
 });
-
 btn03.addEventListener("click", function(){
     window.location = 'index_bonus.html';
 });
-
 btn04.addEventListener("click", function(){
     window.location = 'index_return.html';
 });
 
-// INDEX_FROM.HTML
-
 btn11.addEventListener("click", function(){
-    window.location = 'index_to.html';
+    window.location = 'index_from.html';
 });
-
 btn12.addEventListener("click", function(){
-    window.location = 'index_to.html';
+    window.location = 'index_from.html';
 });
-
 btn13.addEventListener("click", function(){
-    window.location = 'index_to.html';
-});
-
-btn14.addEventListener("click", function(){
-    window.location = 'index_to.html';
-});
-
-btn15.addEventListener("click", function(){
-    window.location = 'index_to.html';
-});
-
-btn16.addEventListener("click", function(){
-    window.location = 'index_to.html';
-});
-
-// INDEX_TO.HTML
-
-btn21.addEventListener("click", function(){
-    window.location = 'index_date.html';
-});
-
-btn22.addEventListener("click", function(){
-    window.location = 'index_date.html';
-});
-
-btn23.addEventListener("click", function(){
-    window.location = 'index_date.html';
-});
-
-btn24.addEventListener("click", function(){
-    window.location = 'index_date.html';
-});
-
-btn25.addEventListener("click", function(){
-    window.location = 'index_date.html';
-});
-
-btn26.addEventListener("click", function(){
-    window.location = 'index_date.html';
-});
-
-// INDEX_DATE.HTML
-
-btn31.addEventListener("click", function(){
-    window.location = 'index_result.html';
-});
-
-btn42.addEventListener("click", function(){
     window.location = 'index_from.html';
 });
 
+
+btn21.addEventListener("click", function(){
+    window.location = 'index_to.html';
+});
+btn22.addEventListener("click", function(){
+    window.location = 'index_to.html';
+});
+btn23.addEventListener("click", function(){
+    window.location = 'index_to.html';
+});
+btn24.addEventListener("click", function(){
+    window.location = 'index_to.html';
+});
+btn25.addEventListener("click", function(){
+    window.location = 'index_to.html';
+});
+btn26.addEventListener("click", function(){
+    window.location = 'index_to.html';
+});
+
+btn31.addEventListener("click", function(){
+    window.location = 'index_date.html';
+});
+btn32.addEventListener("click", function(){
+    window.location = 'index_date.html';
+});
+btn33.addEventListener("click", function(){
+    window.location = 'index_date.html';
+});
+btn34.addEventListener("click", function(){
+    window.location = 'index_date.html';
+});
+btn35.addEventListener("click", function(){
+    window.location = 'index_date.html';
+});
+btn36.addEventListener("click", function(){
+    window.location = 'index_date.html';
+});
+
+btn41.addEventListener("click", function(){
+    window.location = 'index_result.html';
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
     tg.sendData(item);
