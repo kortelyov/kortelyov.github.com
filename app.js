d0f9d0d802d7c5d0c7435c90d65be600555f8d39
Eugene Kortelyov <eugene.kortelyov@gmail.com>
2023-10-19 20:27:56 +0300

    5th commit

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
 btn1.addEventListener("click", function(){
+    window.location = 'cairo.html';
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     }
     else {
-        tg.MainButton.setText("Вы выбрали товар 1!");
+        // tg.MainButton.setText("Вы выбрали товар 1!");
         item = "1";
         tg.MainButton.show();
     }
