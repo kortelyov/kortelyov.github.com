619520e7713aa92bc0ee1f770a5a9800bbb769fa
Роман Сусла <raman_susla@Air-Elena.lan>
2023-10-24 05:36:32 +0300

    fix

--- etickets/tailwind.config.js
+++ etickets/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     './index.html',
@@ -9,15 +10,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'vtd-secondary': colors.neutral, // Light mode Datepicker color
+        'vtd-primary': colors.neutral,
         'telegram-bg': 'var(--tg-theme-bg-color)',
         'telegram-text': 'var(--tg-theme-text-color)',
         'telegram-hint': 'var(--tg-theme-hint-color)',
         'telegram-link': 'var(--tg-theme-link-color)',
         'telegram-primary': 'var(--tg-theme-button-color)',
         'telegram-primary-text': 'var(--tg-theme-button-text-color)',
-        'telegram-secondary-bg': 'var(--tg-theme-secondary-bg-color)',
-        'vtd-primary': 'var(--tg-theme-button-color)',
-        'vtd-secondary': 'var(--tg-theme-secondary-bg-color)'
+        'telegram-secondary-bg': 'var(--tg-theme-secondary-bg-color)'
+        // 'vtd-primary': 'var(--tg-theme-button-color, #f00)',
+        // 'vtd-secondary': 'var(--tg-theme-secondary-bg-color, #0f0)'
       }
     }
   },
